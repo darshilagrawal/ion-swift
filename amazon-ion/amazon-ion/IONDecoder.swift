eaae398bd64679498fb6b10b6d4733e9f5318a2e
darshilagrawal <darshilagrawal2000@gmail.com>
2021-06-29 00:18:38 +0530

    Update IONDecoder.swift
    Dummy Code for IONDecoder

--- amazon-ion/amazon-ion/IONDecoder.swift
+++ amazon-ion/amazon-ion/IONDecoder.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+final class IONDecoder: Decoder {
+    let data: Data? = nil
+    func unkeyedContainer() throws -> UnkeyedDecodingContainer {
+        fatalError()
+    }
+    
+    func singleValueContainer() throws -> SingleValueDecodingContainer {
+        fatalError()
+    }
+    
+    func container<Key>(keyedBy type: Key.Type) throws -> KeyedDecodingContainer<Key> where Key : CodingKey {
+        return KeyedDecodingContainer(KDC(data!))
+    }
+    
+    struct KDC<key: CodingKey>: KeyedDecodingContainerProtocol{
+        var codingPath: [CodingKey] = []
+        var allKeys: [Key] = []
+        let element: Data
+        init(_ element: Data) {
+            self.element = element
+        }
+        func contains(_ key: Key) -> Bool {
+            return data.child(for: key) != nil
+        }
+        func child(for key: CodingKey) throws -> Data{
+            guard let child = data.child(for: key) else{
+                throw DecodingError.keyNotFound(key, DecodingError.Context(codingPath: codingPath, debugDescription: "Error"))
+            }
+        }
+    }
+    
+    
+    
+}
