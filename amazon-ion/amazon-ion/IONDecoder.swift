5e48ff82993f9bd22c949bd889e1dd0aa5cc602c
darshilagrawal <darshilagrawal2000@gmail.com>
2021-06-28 23:35:39 +0530

    Source Files Added
    Adding Basic Implementation files:-
    1) Amazon_ion file which defines the Serialisation language amazon-ion
    2) IONDecoder which will hold the logic for Decoding
    3) IONEncoder which will hold the logic for Encoding

---
+++ amazon-ion/amazon-ion/IONDecoder.swift
@@ -0,0 +1,8 @@
+//
+//  IONDecoder.swift
+//  Amazon-ion
+//
+//  Created by Darshil Agrawal on 28/06/21.
+//
+
+import Foundation
---
+++ amazon-ion/amazon-ion/IONEncoder.swift
@@ -0,0 +1,8 @@
+//
+//  IONEncoder.swift
+//  Amazon-ion
+//
+//  Created by Darshil Agrawal on 28/06/21.
+//
+
+import Foundation
